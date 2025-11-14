fd0a26ba6f8da77abb9adf465fdff6d84e45e56c
aqvoidDev <mindeow35@gmail.com>
2025-11-14 23:15:49 +0200

    Added Hex Directions

---
+++ Assets/HexRoomGenerator/HexDirection.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace HexDungeon
+{
+    public enum HexDirection
+    {
+        NorthEast = 0, East = 1, SouthEast = 2,
+        SouthWest = 3, West = 4, NorthWest = 5
+    }
+
+    public static class HexDirectionExtensions
+    {
+        public static HexDirection Opposite(this HexDirection dir) => (HexDirection)(((int)dir + 3) % 6); 
+        public static HexDirection Next(this HexDirection dir) => (HexDirection)(((int)dir + 1) % 6);
+        public static HexDirection Previous(this HexDirection dir) => (HexDirection)(((int)dir - 1) % 6);
+    }
+}
