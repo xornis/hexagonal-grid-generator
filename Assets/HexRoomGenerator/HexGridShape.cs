c85a0910ddbc2a1439044d5e4fda33f5d25322fc
aqvoidDev <mindeow35@gmail.com>
2025-11-15 15:24:24 +0200

    Added HexGrid and HexGenerator
    - also added hex coord extensions

---
+++ Assets/HexRoomGenerator/HexGridShape.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+
+namespace HexDungeon
+{
+    public static class HexGridShape
+    {
+        
+    }
+}
--- Assets/HexRoomGenerator/HexRoomGenerator.cs
+++ Assets/HexRoomGenerator/HexRoomGenerator.cs
@@ -1,6 +1,28 @@
 using UnityEngine;
 
-public class HexRoomGenerator
+namespace HexDungeon
 {
-    
+    public class HexRoomGenerator : MonoBehaviour
+    {
+        public int radius = 2;
+        public float hexSize = 1f;
+        public GameObject hexPrefab;
+
+
+        private void Start()
+        {
+            foreach (var hex in HexCoord.Zero.Disk(radius))
+            {
+                Vector3 pos = HexToWorld(hex, hexSize);
+                Instantiate(hexPrefab, pos, Quaternion.identity, transform);
+            }
+        }
+
+        private Vector3 HexToWorld(HexCoord h, float size)
+        {
+            float x = size * (1.5f * h.Q);
+            float y = size * (Mathf.Sqrt(3) * (h.R + h.Q * 0.5f));
+            return new Vector3(x, y, 0f);
+        }
+    }
 }
