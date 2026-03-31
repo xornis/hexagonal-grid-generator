a1757b27bb16fa4ab68e1369bb84764bf52083fb
xornis <xornis.dev@gmail.com>
2026-03-31 15:05:15 +0300

    Update HexRoomGeneratorEditor & draw sections

--- Assets/xornis/HexagonalConstructor/Editor/HexRoomGeneratorEditor.cs
+++ Assets/xornis/HexagonalConstructor/Editor/HexRoomGeneratorEditor.cs
@@ -1,4 +1,5 @@
 #if UNITY_EDITOR
+using HexDungeon.Editor;
 using UnityEditor;
 
 namespace HexDungeon
@@ -8,15 +9,29 @@ namespace HexDungeon
     {
         private HexRoomGenerator mainGen;
 
+        private GridSettingsSection gridSettingsSection;
+        private GenerationSettingsSection generationSettingsSection;
+        private PreviewSettingsSection previewSettingsSection;
+        private DebugSettingsSection debugSettingsSection;
+
         private void OnEnable()
         {
             mainGen = (HexRoomGenerator)target;
+
+            gridSettingsSection = new GridSettingsSection(serializedObject);
+            generationSettingsSection = new GenerationSettingsSection(serializedObject);
+            previewSettingsSection = new PreviewSettingsSection(mainGen);
+            debugSettingsSection = new DebugSettingsSection(serializedObject, mainGen);
         }
 
         public override void OnInspectorGUI()
         {
             serializedObject.Update(); // Loading MonoBehaviour fields to SerializedObject
 
+            gridSettingsSection.Draw();
+            generationSettingsSection.Draw();
+            previewSettingsSection.Draw();
+            debugSettingsSection.Draw();
 
             serializedObject.ApplyModifiedProperties(); // Saving changes made in the Inspector
         }
